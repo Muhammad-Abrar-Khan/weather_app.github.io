d00c7595102976bbc8c2be28681a03e09b43e974
Muhammad Abrar Khan <makabrarkhan110@gmail.com>
2023-09-03 17:41:18 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// JavaScript code for handling UI (no API integration)
+
+document.getElementById('searchBtn').addEventListener('click', function () {
+    const cityInput = document.getElementById('cityInput').value;
+    // You can use the entered city name to fetch weather data from an API here.
+    // Update the UI with the fetched data.
+    // Handle errors if the city is not found or if there's a network issue.
+});
+
+// You can add more JavaScript here to handle temperature conversion, optional features, etc.
