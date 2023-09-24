5ab6e8ce8fd580662e1c17a68fa64870463882e0
Muhammad Abrar Khan <makabrarkhan110@gmail.com>
2023-09-24 16:31:44 +0500

    another

--- index.js
+++ index.js
@@ -1,69 +1,56 @@
 const searchButton = document.querySelector('.searchButton');
 const searchInput = document.querySelector('.searchInput');
+const temperatureElement = document.querySelector('.temp');
+const weatherTypeElement = document.querySelector('.weathertype');
+const locationElement = document.querySelector('.location');
+const weatherImage = document.querySelector('.clouds');
+const dayDateElement = document.querySelector('.daydate');
 
-// Function to fetch weather data based on the country code
-function getWeatherData(countryCode) {
-    const apiKey = 'e07a7a6e8c3782dd444bfad6695ef933';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${countryCode}&appid=${apiKey}&units=metric`;
+const apiKey = 'e07a7a6e8c3782dd444bfad6695ef933';
 
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            updateWeatherUI(data);
-        })
-        .catch((error) => {
-            console.error('Error fetching weather data:', error);
-        });
+function getWeatherIconUrl(iconCode) {
+    return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 }
 
-// Event listener for the search button click
-searchButton.addEventListener('click', function () {
-    const countryCode = searchInput.value;
-    getWeatherData(countryCode);
-});
+async function getWeatherData(countryCode) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${countryCode}&appid=${apiKey}&units=metric`;
 
-// Event listener for the Enter key press in the input field
-searchInput.addEventListener('keyup', function (event) {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        updateWeatherUI(data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+
+function updateWeatherUI(data) {
+    const weatherDescription = data.weather[0].description;
+    const iconCode = data.weather[0].icon;
+    const iconSrc = getWeatherIconUrl(iconCode);
+
+    temperatureElement.textContent = `${data.main.temp}°C`;
+    weatherTypeElement.textContent = weatherDescription;
+    locationElement.textContent = data.name;
+    weatherImage.src = iconSrc;
+    weatherImage.style.height = '7rem';
+    weatherImage.style.width = '7rem';
+}
+
+
+searchButton.addEventListener('click', () => getWeatherData(searchInput.value));
+
+searchInput.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
-        const countryCode = searchInput.value;
-        getWeatherData(countryCode);
+        getWeatherData(searchInput.value);
     }
 });
 
-function updateWeatherUI(data) {
-    const temperatureElement = document.querySelector('.temp');
-    const weatherTypeElement = document.querySelector('.weathertype');
-    const locationElement = document.querySelector('.location');
+dayDateElement.textContent = formatDate(new Date());
 
-    const temperature = data.main.temp;
-    const weatherType = data.weather[0].description;
-    const cityName = data.name;
-
-    temperatureElement.textContent = `${temperature}°C`;
-    weatherTypeElement.textContent = weatherType;
-    locationElement.textContent = cityName;
-}
-
-// Function to format a date as "Day, DD Month" (e.g., "Fri, 05 June")
 function formatDate(date) {
     const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const months = [
-        'January', 'February', 'March', 'April',
-        'May', 'June', 'July', 'August',
-        'September', 'October', 'November', 'December'
-    ];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    const dayOfWeek = days[date.getDay()];
-    const dayOfMonth = date.getDate();
-    const month = months[date.getMonth()];
-
-    return `${dayOfWeek}, ${dayOfMonth} ${month}`;
+    return `${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]}`;
 }
-
-// Usage example:
-const today = new Date();
-const formattedDate = formatDate(today);
-
-// Update the HTML element with class "daydate" with the formatted date
-const dayDateElement = document.querySelector('.daydate');
-dayDateElement.textContent = formattedDate;
